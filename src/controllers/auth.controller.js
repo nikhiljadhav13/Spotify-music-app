f93aafa4c151ee7599a4a370a70d7017e535d042
nikhiljadhav13 <115921099+nikhiljadhav13@users.noreply.github.com>
2026-06-09 21:22:05 +0530

    creatd login api

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,29 +1,29 @@
 // isme hum api ka logic likhenge, jaise ki login, register, logout, etc.
 
 const userModel = require('../models/user.models');
-const jwt = require('jsonwebtoken');        
+const jwt = require('jsonwebtoken'); 
+const bcrypt = require('bcryptjs');       
 
 async function registerUser(req,res){
 
     const {username,email,password,role="user"}= req.body;  // yeh line req.body se username, email, password aur role ko extract kar raha hai. Agar role nahi diya gaya hai toh default value "user" set kar di jayegi.
-
-    const isUserAlreadyExist = await userModel.findOne(
-         
+    const isUserAlreadyExist = await userModel.findOne( 
        { $or:[                              // $or operator MongoDB ka ek logical operator hai jo multiple conditions ko check karta hai. Agar inme se koi bhi condition true hoti hai toh woh document match ho jata hai.
             {email: email},
             {username: username}
         ]}
-
     ); 
     if (isUserAlreadyExist) {
         return res.status(409).json({message: "User already exists"});
     }
+    const hash = await bcrypt.hash(password,10);  // bcrypt.hash method password ko hash karta hai. Isme hum password aur salt rounds (10) pass kar rahe hain. Salt rounds hashing process ko aur secure banata hai.
     const User = await userModel.create({  // create method userModel ka ek static method hai jo ek naya document create karta hai aur usse database me save karta hai. Isme hum username, email, password aur role pass kar rahe hain.
         username,
         email,
-        password,
+        password: hash,
         role
     });
+
 
     const token = jwt.sign({
         id: User._id,
@@ -43,3 +43,46 @@ async function registerUser(req,res){
         }
     })
 }
+
+async function loginUser(req,res){
+
+    const { username , email , password } =req.body;  // yeh line req.body se username, email aur password ko extract kar raha hai.
+
+    const user = await userModel.findOne({
+        $or:[
+            {username: username},
+            {email: email}
+        ]
+    })
+
+    if (!user){
+        return res.status(401).json({
+            message: "invalid credentials"
+        })
+    }
+
+    const isPasswordisValid = await bcrypt.compare(password, user.password);  // bcrypt.compare method plaintext password ko hashed password ke sath compare karta hai. Isme hum user se input password aur database me stored hashed password pass kar rahe hain.
+    if (!isPasswordisValid){
+        return res.status(401).json({
+            message: "invalid credentials"
+        })
+    }
+
+    const token = jwt.sign({
+        id : user._id,
+        role: user.role
+    },process.env.JWT_SECRET);
+
+    res.cookie("token", token);
+    res.status(200).json({
+        message: "login successful",
+        user:{
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            role: user.role
+        }
+    });
+}
+
+module.exports ={registerUser, loginUser}
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,13 +1,15 @@
 // this folder will contain all the routes related to authentication like register, login, logout, etc.
 
 const express = require('express');
-const jwt = require('jsonwebtoken');
-const userModel = require('../models/user.models');
+const authController = require('../controllers/auth.controller')
+
 
 const router = express.Router();
 
 // Register route
-router.post('/register', async (req, res) => {});
+router.post('/register', authController.registerUser);  // yeh line POST request ke liye /register route define kar rahi hai. Jab bhi koi client /register endpoint par POST request bhejega, toh authController ke registerUser function ko call kiya jayega.
+
+router.post('/login', authController.loginUser);  // yeh line POST request ke liye /login route define kar rahi hai. Jab bhi koi client /login endpoint par POST request bhejega, toh authController ke loginUser function ko call kiya jayega.
 
 
 
