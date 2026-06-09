96f135b347d531e1fe6d254a066df198564b905a
nikhiljadhav13 <115921099+nikhiljadhav13@users.noreply.github.com>
2026-06-09 13:32:22 +0530

    add token and api logic

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 // isme hum api ka logic likhenge, jaise ki login, register, logout, etc.
 
 const userModel = require('../models/user.models');
+const jwt = require('jsonwebtoken');        
 
 async function registerUser(req,res){
 
@@ -17,10 +18,28 @@ async function registerUser(req,res){
     if (isUserAlreadyExist) {
         return res.status(409).json({message: "User already exists"});
     }
-    const newUser = await userModel.create({  // create method userModel ka ek static method hai jo ek naya document create karta hai aur usse database me save karta hai. Isme hum username, email, password aur role pass kar rahe hain.
+    const User = await userModel.create({  // create method userModel ka ek static method hai jo ek naya document create karta hai aur usse database me save karta hai. Isme hum username, email, password aur role pass kar rahe hain.
         username,
         email,
         password,
         role
     });
+
+    const token = jwt.sign({
+        id: User._id,
+        role: User.role
+
+    },process.env.JWT_SECRET);  // jwt.sign method JWT token generate karta hai. Isme hum payload (id aur role) aur secret key pass kar rahe hain.
+
+    res.cookie("token",token); // res.cookie method HTTP response me cookie set karta hai. Isme hum cookie ka naam "token" aur uski value token pass kar rahe hain.
+
+    res.status(201).json({
+        message:"user registered successfully",
+        User:{
+            id: User._id,
+            username: User.username,
+            email: User.email,
+            role: User.role
+        }
+    })
 }
